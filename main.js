5653d5ccf776ad90cb32479535f95bb4ac500237
Cesar Martinez <cesarmartinez99@gmail.com>
2025-02-05 22:02:10 -0500

    ACTUALIZA MAIN

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import * as XLSX from "https://cdn.jsdelivr.net/npm/xlsx/xlsx.mjs";
 const form = document.getElementById("registro-form");
 const descargarBtn = document.getElementById("descargar");
 const borrarBtn = document.getElementById("borrar");
-
 let registros = []; // Array para almacenar los datos del formulario
 
 // Recupera los registros guardados en LocalStorage al cargar la página
@@ -15,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
 // Evento para enviar los datos del formulario y añadirlos al array
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const data = {
     usuario: form.usuario.value,
     tipoId: form["tipo-id"].value,
@@ -24,12 +22,9 @@ form.addEventListener("submit", (e) => {
     fecha: form.fecha.value,
     observacion: form.observacion.value,
   };
-
   registros.push(data); // Añadir datos al array
-
   // Guardar los registros actualizados en LocalStorage
   localStorage.setItem("registros", JSON.stringify(registros));
-
   form.reset();
   Swal.fire("¡Genial!", "El registro se guardó correctamente.", "success");
 });
@@ -40,11 +35,9 @@ descargarBtn.addEventListener("click", () => {
     Swal.fire("Advertencia", "No hay datos para descargar.", "warning");
     return;
   }
-
   const worksheet = XLSX.utils.json_to_sheet(registros); // Crear hoja de cálculo
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "Registros");
-
   XLSX.writeFile(workbook, "registros_inhouse.xlsx"); // Descargar archivo
   Swal.fire("¡Listo!", "El archivo Excel se descargó correctamente.", "success");
 });
